16851149972a5e70dc34ebd47ac1e44236b60cd4
Ari Majumdar <akm303@gwmail.gwu.edu>
2025-11-28 17:31:14 -0500

    moved training files to agents/training directory, and param outputs/inputs to agents/params

--- train.py
+++ train.py
@@ -1,6 +1,11 @@
-from MCTS.algorithm import MCTS
+# from MCTS.algorithm import MCTS
+# from agents.training.train_MCTS import train
+from agents.training.train_Q import train
+
+AGENT_DIR = 'agents/'
+PARAM_DIR = 'agents/params/'
+
+# AGENT_TO_TRAIN = MCTS()
 
 if __name__ == '__main__':
-    mcts = MCTS()
-    for _ in range(10):
-        mcts.one_iteration()
+    train()
